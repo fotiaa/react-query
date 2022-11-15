10182147946181099cb6bc1ceb23d5128ac412c6
hariom sharma <hariomsharma@harioms-MacBook-Pro.local>
2022-11-15 09:27:38 +0530

    Added lec 4 handling query error

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,33 @@
-export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
+import { useQuery } from "react-query"
+import axios from "axios"
+
+
+const fetchSuperHeroes = async () => {
+  return axios.get('http://localhost:4000/wrong_url')
 }
+
+export const RQSuperHeroesPage = () => {
+  // first argument inside useQuery is a unique key and the second arguemnt is a fat arrow function that returns a promise.
+  const {isLoading, data, isError, error } = useQuery('super-heroes', fetchSuperHeroes)
+
+  if(isLoading){
+    return <h2>Loading...</h2>
+  }
+
+  if(isError){
+    return <h2>{error.message}</h2>
+  }
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      {
+        data?.data.map(hero =>{
+          return <div key={hero.name}>
+            {hero.name}
+          </div>
+        })
+      }
+    </>
+  )
+}
+
