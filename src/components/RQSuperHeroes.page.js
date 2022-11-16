dabc103e15e3f246d2526feace9145a969098d01
hariom sharma <hariomsharma@harioms-MacBook-Pro.local>
2022-11-16 22:11:26 +0530

    Added lec 7 stale time concept

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -8,7 +8,14 @@ const fetchSuperHeroes = async () => {
 
 export const RQSuperHeroesPage = () => {
   // first argument inside useQuery is a unique key and the second arguemnt is a fat arrow function that returns a promise.
-  const {isLoading, data, isError, error } = useQuery('super-heroes', fetchSuperHeroes)
+  const {isLoading, data, isError, error } = useQuery('super-heroes', fetchSuperHeroes,
+    {
+      staleTime:30000
+    }
+  )
+// Stale time is set to 30s seconds here. In the last scenario we have seen isFetching was true after rendering
+// RQSuperHeroesPage component for the second time but when we stale time as 30s then no re-fetching will occur in the 
+// background for 30s after this component is re-rendered for the second time.
 
   if(isLoading){
     return <h2>Loading...</h2>
