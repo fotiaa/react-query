fb217d7b71825e16be6600cc3580f6a5d8842f44
hariom sharma <hariomsharma@harioms-MacBook-Pro.local>
2022-11-17 23:14:22 +0530

    Added lec 10 useQuery on click

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,7 @@
+// step 1 : This is set to to false to inform useQuery not to fetch the data when the component mounts. 
+// step 2 : useQuery returns a function called as refetch to manually trigger the query. All we have to do is pass it in as 
+//          onClick handler.
+
 import { useQuery } from "react-query"
 import axios from "axios"
 
@@ -7,18 +11,12 @@ const fetchSuperHeroes = async () => {
 }
 
 export const RQSuperHeroesPage = () => {
-// first argument inside useQuery is a unique key and the second arguemnt is a fat arrow function that returns a promise.
-// Third argument is an object where we can configure multiple properties in which cache time is one of them.
-  const {isLoading, data, isError, error, isFetching } = useQuery('super-heroes', fetchSuperHeroes)
 
-// when RQSuperHeroesPage is rendered first isLoading and isFetching both them are true when the fetching is complete both are set
-// to false. If we try re-render this component then this time isLoading is false then isFetching is true, when the fetching 
-// gets completed isLoading and isFetching both are set to true and if the data gets updated between the first and second
-// render then isFetching updates the changed data, that's why during second rendering isFetching is true while isLoading
-// is true as React query has inbuilt cache memory(by default 5 minutes).
-
-//
-  console.log({isLoading, isFetching});
+  const {isLoading, data, isError, error, isFetching , refetch} = useQuery('super-heroes', fetchSuperHeroes,
+    {
+      enabled:false
+    }
+  )
 
   if(isLoading){
     return <h2>Loading...</h2>
@@ -30,13 +28,12 @@ export const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>React Query Super Heroes Page</h2>
-      {
-        data?.data.map(hero =>{
-          return <div key={hero.name}>
-            {hero.name}
-          </div>
-        })
-      }
+      <button onClick={refetch}> Fetch Heroes </button>
+      {data?.data.map(hero =>{
+        return <div key={hero.name}>
+          {hero.name}
+        </div>
+      })}
     </>
   )
 }
